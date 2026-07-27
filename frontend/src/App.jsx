17f01aef5b968b8d667f88dde592a52682fe020d
Natars84 <pasero.romain@gmail.com>
2026-07-27 11:33:38 +0200

    Enveloppe l'application avec BlockTypesProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { BlockTypesProvider } from "./context/BlockTypesContext";
 import FormsList from "./pages/FormsList";
 import FormHub from "./pages/FormHub";
 import FormBuilder from "./pages/FormBuilder";
@@ -6,11 +7,13 @@ import FormPreview from "./pages/FormPreview";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<FormsList />} />
-      <Route path="/forms/:formId" element={<FormHub />} />
-      <Route path="/forms/:formId/builder" element={<FormBuilder />} />
-      <Route path="/forms/:formId/preview" element={<FormPreview />} />
-    </Routes>
+    <BlockTypesProvider>
+      <Routes>
+        <Route path="/" element={<FormsList />} />
+        <Route path="/forms/:formId" element={<FormHub />} />
+        <Route path="/forms/:formId/builder" element={<FormBuilder />} />
+        <Route path="/forms/:formId/preview" element={<FormPreview />} />
+      </Routes>
+    </BlockTypesProvider>
   );
 }
